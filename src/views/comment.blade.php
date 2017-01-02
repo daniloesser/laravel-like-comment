21efe2b074d15a6b093dad516a18a12a12bbbd02
daniloesser <daniloesser@gmail.com>
2017-01-02 17:00:10 -0200

    Ajustes - Brewme

--- src/views/comment.blade.php
+++ src/views/comment.blade.php
@@ -5,16 +5,16 @@ if(!Auth::check())
 $GLOBALS['commentClass'] = -1;
 ?>
 <div class="laravelComment" id="laravelComment-{{ $comment_item_id }}">
-    <h3 class="ui dividing header">Comments</h3>
+    <h4 class="ui dividing header">Coment&aacute;rios e Avalia&ccedil;&otilde;es</h4>
     <div class="ui threaded comments" id="{{ $comment_item_id }}-comment-0">
         <form class="ui laravelComment-form form" id="{{ $comment_item_id }}-comment-form" data-parent="0" data-item="{{ $comment_item_id }}">
             <div class="field">
-                <textarea id="0-textarea" rows="2" {{ $GLOBALS['commentDisabled'] }}></textarea>
+                <textarea id="0-textarea" rows="2" {{ $GLOBALS['commentDisabled'] }} placeholder="Como foi sua experiência? Compartilhe conosco!"></textarea>
                 @if(!Auth::check())
-                    <small>Please Log in to comment</small>
+                    <small>É preciso estar logado para realizar avaliações.</small>
                 @endif
             </div>
-            <input type="submit" class="ui basic small submit button" value="Comment" {{ $GLOBALS['commentDisabled'] }}>
+            <input type="submit" class="ui basic small submit button" value="Salvar" {{ $GLOBALS['commentDisabled'] }}>
         </form>
 <?php
 $GLOBALS['commentVisit'] = array();
@@ -30,23 +30,29 @@ function dfs($comments, $comment){
         <div class="content">
             <a class="author" url="{{ $comment->url or '' }}"> {{ $comment->name }} </a>
             <div class="metadata">
-                <span class="date">{{ $comment->updated_at->diffForHumans() }}</span>
+                <span class="date">em <i>{!! Carbon\Carbon::parse($comment->created_at)->format('d/m/Y à\s h:i:s') !!}</i></span>
             </div>
             <div class="text">
                 {{ $comment->comment }}
             </div>
-            <div class="actions">
-                <a class="{{ $GLOBALS['commentDisabled'] }} reply reply-button" data-toggle="{{ $comment->id }}-reply-form">Reply</a>
+            <div class="actions grouped">
+                {{ \risul\LaravelLikeComment\Controllers\CommentController::viewLike('comment-'.$comment->id) }}
+                @if($comment->user_id != Auth::user()->id)
+                    <a class="{{ $GLOBALS['commentDisabled'] }} reply reply-button btn btn-outline btn-xs " data-toggle="{{ $comment->id }}-reply-form" title="Responder"><i class="large icon reply"></i></a>
+                @else
+                    <a class="delete delete-button btn btn-outline btn-xs" data-id="{{ $comment->id }}" data-item-type="comment" title="Apagar"><i class="large icon trash"></i></a>
+                @endif
+
             </div>
-            {{ \risul\LaravelLikeComment\Controllers\CommentController::viewLike('comment-'.$comment->id) }}
+
             <form id="{{ $comment->id }}-reply-form" class="ui laravelComment-form form" data-parent="{{ $comment->id }}" data-item="{{ $comment->item_id }}" style="display: none;">
                 <div class="field">
                     <textarea id="{{ $comment->id }}-textarea" rows="2" {{ $GLOBALS['commentDisabled'] }}></textarea>
                     @if(!Auth::check())
-                        <small>Please Log in to comment</small>
+                        <small>É preciso estar logado para realizar avaliações.</small>
                     @endif
                 </div>
-                <input type="submit" class="ui basic small submit button" value="Comment" {{ $GLOBALS['commentDisabled'] }}>
+                <input type="submit" class="ui basic small submit button" value="Salvar" {{ $GLOBALS['commentDisabled'] }}>
             </form>
         </div>
         <div class="comments" id="{{ $comment->item_id }}-comment-{{ $comment->id }}">
@@ -68,5 +74,4 @@ foreach ($comments as $comment) {
 }
 ?>
     </div>
-    <button class="ui basic button" id="showComment" data-show-comment="0" data-item-id="{{ $comment_item_id }}">Show comments</button>
 </div>
